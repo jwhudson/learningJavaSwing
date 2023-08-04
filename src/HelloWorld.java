79424cf931084e2165774441b2d88da79db5d043
jwhudson <44750919+jwhudson@users.noreply.github.com>
2023-08-04 21:18:37 +0100

    First label

---
+++ src/HelloWorld.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class HelloWorld {
+    private JPanel rootPanel;
+    private JLabel helloLabel;
+    private JButton helloButton;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("HelloWorld");
+        frame.setContentPane(new HelloWorld().rootPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public HelloWorld() {
+        helloButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                helloLabel.setText("Hello World");
+            }
+        });
+    }
+}
